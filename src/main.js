6669d66e0e886bdc5a46c808f49151e33fd9abbe
lixiaoqiang <gzhidao1010@gmail.com>
2018-08-01 10:11:05 +0800

    新增阿里图标

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/iconfont/iconfont.css'
 import 'normalize.css'
 import './assets/css/reset.less'
 // import 'lib-flexible/flexible.js'
