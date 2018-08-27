c4e64e1b6f051809ba235c6948dab77a2b9d425e
wujian <a123648305@126.com>
2018-08-27 10:33:14 +0800

    1

--- src/components/common/commonNoData/index.js
+++
@@ -1,8 +0,0 @@
-import commonNoData from './src/main';
-
-/* istanbul ignore next */
-commonNoData.install = function (Vue) {
-    Vue.component(commonNoData.name, commonNoData);
-};
-
-export default commonNoData;
