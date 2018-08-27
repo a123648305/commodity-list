0d047e4dd479324b67189d6224d16f9680f40516
wujian <a123648305@126.com>
2018-08-27 10:34:16 +0800

    2

---
+++ src/pages/projectSchedule/template/plan/index.vue
@@ -0,0 +1,323 @@
+<style lang="less" scoped>
+// 项目规划
+.projectSchedule-plan-index {
+    background-color: #000;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    letter-spacing: 2px;
+    overflow: hidden;
+    .plan-title {
+    position: relative;
+    width: 100%;
+    text-align: center;
+    color: #fff;
+    font-size: 60px;
+    //box-sizing: border-box;
+    padding: 50px 0px;
+    letter-spacing: 3px;
+    //height: 120px;
+    i {
+        transform: rotate(180deg);
+        float: left;
+        font-size: 50px;
+        margin-left: 50px;
+
+    }
+    span {
+        margin-left: -50px;
+    }
+
+    }
+    .plan-list {
+        //background-color: #000;
+        height: 100%;
+        overflow-y: auto;
+        overflow-x: hidden;
+        &::-webkit-scrollbar {
+            display:none
+        } 
+        li {
+            //background-color:greenyellow;
+            list-style-type: none;
+            padding-left:30px;
+            padding-right:40px;
+            font-size: 50px; 
+            display: flex;
+            align-items: center;
+            width: 100%;
+            .uptime{
+                text-align: center;
+            }
+            .uptime-content {
+                color: #bdbdbd;
+                font-size: 30px;
+                margin-top: 16px;
+                text-align: center;
+                transform: scale(0.8);
+
+            }
+            .upname {
+                float: left;
+                width: 80%;
+               
+            }
+            .upname-p1,
+            .upname-p2 {
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+            }
+            .upname-p1 {
+                color: #fff;
+                font-size: 50px;
+                letter-spacing: 3px;
+
+            }
+            .upname-p2 {
+                color: #bdbdbd;
+                font-size: 44px;
+                margin-top: 47px;
+
+            }
+
+
+
+            .delay-time {
+                float: right;
+                text-align: center;
+                margin-right: -30px;
+                letter-spacing: 3px;
+                padding-top: 17px;
+            }
+
+            .delay-time-number {
+                font-Size:60px;
+                color: #19c8a9;
+            }
+            .delay-time-day {
+                font-size: 50px;
+                font-weight: normal;
+                margin-left: -15px;
+            }
+            .delay-time-text {
+                color: #bdbdbd;
+                font-size: 30px;
+                margin-top: 15px;
+                
+
+
+            }
+
+            .plan-right {
+                margin-right: 41px;
+                padding-top: 30px;
+            }
+            .iconfont {
+                color: #916b05;
+                font-size: 40px;
+            }
+
+        }
+        .uptime-date {
+            color: #fff;
+            letter-spacing: 3px;
+            font-size: 50px;
+            &::before {
+                content: "#";
+                font-size: 30px;
+            }
+        }
+        .upcontent {
+            //background-color: #f5a623;
+            border-bottom: 1px solid #946c05; /*no*/
+            //height:118px;
+            float: left;
+            margin-left: 74px;
+            margin-right: 50px;
+            width: 63%;
+            padding-top: 50px;
+            padding-bottom: 50px;
+            @media screen and(max-width: 320px) {
+                width: 57%;
+            }
+            @media screen and(max-width: 375px)and(min-width: 330px) {
+                width: 61%;
+            }    
+        }
+    }
+    
+       
+}
+    
+</style>
+
+<template>
+    <div>
+        <!-- 项目计划 -->
+        <div class="projectSchedule-plan-index">
+            <div class="plan-title">
+                <i class="iconfont icon-you" @click="black"></i>
+                <span>小红十项目研发规划</span>
+            </div>
+            <div class="plan-list">
+
+                <ul>
+                    <li v-for="item in 10" :key="item">
+                        <div class="uptime">
+                            <p class="uptime-date">8月10日</p>
+                            <p class="uptime-content">测试上线时间</p>
+                        </div>
+                        <div class="upcontent">
+                            <div class="upname">
+                                <p class="upname-p1">小红十V1.0.0</p>
+                                <p class="upname-p2">新版后台、手机端、小程序</p>
+                            </div>
+                            <div class="delay-time">
+                                <p class="delay-time-number">28
+                                    <b class="delay-time-day">天</b>
+                                </p>
+                                <p class="delay-time-text">倒计时</p>
+
+                            </div>
+
+                        </div>
+                        <div class="plan-right">
+                            <i class="iconfont icon-you "></i>
+                        </div>
+
+
+                    </li>
+
+                    <li>
+                        <div class="uptime">
+                            <p class="uptime-date">8月10日</p>
+                            <p class="uptime-content">测试上线时间</p>
+                        </div>
+                        <div class="upcontent">
+                            <div class="upname">
+                                <p class="upname-p1">小红十V1.0.0</p>
+                                <p class="upname-p2">新版后台、手机端、小程序</p>
+                            </div>
+                            <div class="delay-time">
+                                <p class="delay-time-number">28
+                                    <b class="delay-time-day">天</b>
+                                </p>
+                                <p class="delay-time-text">倒计时</p>
+
+                            </div>
+
+                        </div>
+                        <div class="plan-right">
+                            <i class="iconfont icon-you "></i>
+                        </div>
+
+
+                    </li>
+                    <li>
+                        <div class="uptime">
+                            <p class="uptime-date">8月10日</p>
+                            <p class="uptime-content">测试上线时间</p>
+                        </div>
+                        <div class="upcontent">
+                            <div class="upname">
+                                <p class="upname-p1">小红十V1.0.0</p>
+                                <p class="upname-p2">新版后台、手机端、小程序</p>
+                            </div>
+                            <div class="delay-time">
+                                <p class="delay-time-number">28
+                                    <b class="delay-time-day">天</b>
+                                </p>
+                                <p class="delay-time-text">倒计时</p>
+
+                            </div>
+
+                        </div>
+                        <div class="plan-right">
+                            <i class="iconfont icon-you "></i>
+                        </div>
+
+
+                    </li>
+                    <li>
+                        <div class="uptime">
+                            <p class="uptime-date">8月10日</p>
+                            <p class="uptime-content">测试上线时间</p>
+                        </div>
+                        <div class="upcontent">
+                            <div class="upname">
+                                <p class="upname-p1">小红十V1.0.0</p>
+                                <p class="upname-p2">新版后台、手机端、小程序</p>
+                            </div>
+                            <div class="delay-time">
+                                <p class="delay-time-number">28
+                                    <b class="delay-time-day">天</b>
+                                </p>
+                                <p class="delay-time-text">倒计时</p>
+
+                            </div>
+
+                        </div>
+                        <div class="plan-right">
+                            <i class="iconfont icon-you "></i>
+                        </div>
+
+
+                    </li>
+                    <li>
+                        <div class="uptime">
+                            <p class="uptime-date">8月10日</p>
+                            <p class="uptime-content">测试上线时间</p>
+                        </div>
+                        <div class="upcontent">
+                            <div class="upname">
+                                <p class="upname-p1">小红十V1.0.0</p>
+                                <p class="upname-p2">新版后台、手机端、小程序</p>
+                            </div>
+                            <div class="delay-time">
+                                <p class="delay-time-number">28
+                                    <b class="delay-time-day">天</b>
+                                </p>
+                                <p class="delay-time-text">倒计时</p>
+
+                            </div>
+
+                        </div>
+                        <div class="plan-right">
+                            <i class="iconfont icon-you "></i>
+                        </div>
+
+
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+    export default {
+
+        data() {
+            return {
+
+            }
+        },
+        computed: {
+
+        },
+        mounted() {
+
+        },
+        methods: {
+                black() {
+            
+            this.$router.go(-1)
+        }
+
+        },
+    }
+</script>
---
+++ src/pages/projectSchedule/template/recent/recent.vue
@@ -0,0 +1,306 @@
+<style lang="less" scoped>
+// 近期
+.projectSchedule-recent-index {
+        background-color: #000;
+    .recent-delay {
+        letter-spacing: 2px;
+        .recent-delay-title {
+            color: #fff;
+            border-left: 20px solid #f5a623;
+            padding-left: 52px;
+            font-size: 48px;
+            height: 60px;
+            line-height:60px;
+            
+        }
+    }
+    .recent-plan {
+        margin-top: 72px;
+        .recent-plan-title {
+            color: #fff;
+            border-left: 20px solid #f5a623;
+            padding-left: 52px;
+            font-size: 48px;
+            height: 60px;
+            line-height:60px;
+        }
+        li {
+            .plan-time-nubmer {
+                font-Size: 60px;
+                color: #19c8a9;
+            }
+            .plan-time-day {
+                font-size: 50px;
+                font-weight: normal;
+                margin-left: -15px;
+            }
+
+            .plan-time-text {
+                color: #bdbdbd;
+                font-size: 30px;
+                margin-top: 15px;
+
+            }
+
+
+
+
+        }
+
+    }
+
+
+    ul {
+        width: 100%;
+        li {
+            list-style-type: none;
+            padding-left:30px;
+            padding-right:40px;
+            //font-size: 50px;
+            display: flex;
+            align-items: center;
+            width: 100%;
+            
+            .uptime {
+                text-align: center;
+            }
+            
+            .uptime-text {
+                color: #bdbdbd;
+                font-size: 30px;
+                margin-top: 16px;
+                letter-spacing: 2px;
+                margin-left: 11px;
+                transform: scale(0.8)
+                
+            }
+            .upname {
+                float: left;
+                display:table-cell;
+                vertical-align: middle; 
+                width: 80%;
+                
+            }
+            .upname-p1,
+            .upname-p2 {
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+            }
+            .upname-p1 {
+                color: #fff;
+                letter-spacing: 3px;
+
+            }
+            .upname-p2 {
+                color: #bdbdbd;
+                font-size: 44px;
+                margin-top: 47px;
+
+            }
+
+            .delay-time {
+                float: right;
+                margin-right: -15px;
+                text-align: center;
+                letter-spacing: 3px;
+                padding-top: 17px;
+            }
+            .plan-time{
+                 margin-right: -30px;
+            }
+            .plan-time-nubmer {
+                font-Size: 60px;
+                color: #ff6767;
+            }
+            .plan-time-day {
+                font-size: 50px;
+                font-weight: normal;
+                margin-left: -15px;
+            }
+
+            .plan-time-text {
+                color: #bdbdbd;
+                font-size: 30px;
+                margin-top:15px;
+
+            }
+
+           
+        }
+        .upcontent {
+            //background-color:#916b05;
+            border-bottom: 1px solid #946c05;/*no*/
+            margin-left: 74px;
+            margin-right:50px;
+            width: 63%;
+            padding-bottom: 50px;
+            padding-top: 50px;
+            display:table;
+            vertical-align: middle;
+            font-size: 50px;
+            @media screen and(max-width: 320px) {
+                width: 57%;
+            }
+            @media screen and(max-width: 375px)and(min-width: 330px) {
+                width: 61%;
+            }                      
+            
+        }
+        .uptime-date {
+            font-size:50px;
+            color: #fff;
+            letter-spacing: 3px;
+            &::before {
+                content: "#";
+                font-size:30px;
+            }
+        }
+        .recent-right {
+            //margin-left:23px;
+            margin-right: 40px;
+            padding-top: 30px;
+            
+            i {
+                color: #916b05;
+                font-size: 40px;
+            }
+        }
+    }
+}
+</style>
+
+<template>
+    <div>
+        <!-- 近期-->
+        <div class="projectSchedule-recent-index">
+            <!-- 上线延误 -->
+            <div class="recent-delay">
+                <div class="recent-delay-title">
+                    <span>上线延误</span>
+                </div>
+
+                <ul>
+                    <li>
+                        <div class="uptime">
+                            <p class="uptime-date">8月10日</p>
+                            <p class="uptime-text">原定测试上线</p>
+                        </div>
+                        <div class="upcontent">
+                            <div class="upname">
+                                <p class="upname-p1">小红十V1.0.0</p>
+                                <p class="upname-p2">新版后台、手机端、小程序</p>
+                            </div>
+                            <div class="delay-time">
+                                <p class="plan-time-nubmer">4
+                                    <b class="plan-time-day">天</b>
+                                </p>
+                                <p class="plan-time-text">延误</p>
+
+                            </div>
+
+                        </div>
+                        <div class="recent-right">
+
+                            <router-link to="/detail-Delay">
+                                <i class="iconfont icon-you "></i>
+                            </router-link>
+
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="uptime">
+                            <p class="uptime-date">8月10日</p>
+                            <p class="uptime-text">原定测试上线</p>
+                        </div>
+                        <div class="upcontent">
+                            <div class="upname">
+                                <p class="upname-p1">小红十V1.0.0</p>
+                                <p class="upname-p2">新版后台、手机端、小程序</p>
+                            </div>
+                            <div class="delay-time ">
+                                <p class="plan-time-nubmer">4
+                                    <b class="plan-time-day">天</b>
+                                </p>
+                                <p class="plan-time-text">延误</p>
+
+                            </div>
+
+                        </div>
+                        <div class="recent-right">
+
+                            <router-link to="/">
+                                <i class="iconfont icon-you "></i>
+                            </router-link>
+
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+
+            <!-- 上线计划 -->
+            <div class="recent-plan">
+                <div class="recent-plan-title">
+                    <span>近期上线计划</span>
+                </div>
+
+                <ul>
+
+
+                    <li v-for="item in 10" :key="item">
+                        <div class="uptime">
+                            <p class="uptime-date">8月10日</p>
+                            <p class="uptime-text">测试上线时间</p>
+                        </div>
+                        <div class="upcontent">
+                            <div class="upname">
+                                <p class="upname-p1">小红十V1.0.0</p>
+                                <p class="upname-p2">手机端</p>
+                            </div>
+                            <div class="delay-time plan-time">
+                                <p class="plan-time-nubmer">31
+                                    <b class="plan-time-day">天</b>
+                                </p>
+                                <p class="plan-time-text">倒计时</p>
+
+                            </div>
+
+                        </div>
+                        <div class="recent-right">
+                            <router-link to="/detail-Countdown">
+                                <i class="iconfont icon-you "></i>
+                            </router-link>
+                        </div>
+
+
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+    export default {
+
+        data() {
+            return {
+
+            }
+        },
+        computed: {
+
+        },
+        mounted() {
+
+        },
+        methods: {
+
+        },
+    }
+</script>
